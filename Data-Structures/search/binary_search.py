0ae39afbd491fd0bf5f87492381f33c96defc37a
Sidharth <sidharth.satapathy9@gmail.com>
2018-02-03 23:23:17 +0530

    First commit with sequential search and binary search

---
+++ Data-Structures/search/binary_search.py
@@ -0,0 +1,22 @@
+def binary_search(arr,ele):
+    found = False
+    first = 0
+    last = len(arr)-1
+
+    while first <= last and not found:
+
+        mid = (first+last)/2
+        if mid == ele:
+            found = True
+        else:
+            if mid <= ele:
+                last = mid-1
+            else: 
+                first = mid+1
+    return found
+
+a = raw_input("Enter the array elememnts separated by space :")
+a = [int(a) for x in a.split(" ")]
+element = int(raw_input("Enter element to be searched :"))
+
+print binary_search(a,element)                
---
+++ Data-Structures/search/sequetial_search.py
@@ -0,0 +1,44 @@
+def seq_search(arr,ele):
+    found = False
+    i = 0
+
+    while i < len(arr) and not found:
+        if arr[i] == ele:
+            found = True
+        else :
+            i+=1
+    return found            
+
+def ordered_seq_search(arr,ele):
+    found = False
+    stopped = False
+    i = 0
+
+    while i < len(arr) and not found and not stopped:
+        if arr[i] == ele:
+            found = True
+        else :
+            if arr[i] > ele:
+                stopped= True
+            else :
+                i+=1
+    return found            
+
+
+
+array = raw_input("Enter the array elements separated by space :")
+array = [int(x) for x in array.split(" ")]
+element = int(raw_input("Enter the element to be search :"))
+print "1 - Unordered array"
+print "2 - Ordered array"
+choice = int(raw_input("Enter choice :"))
+if choice == 1:
+    print seq_search(array,element)
+elif choice == 2:
+    print ordered_seq_search(array,element)
+else:
+    print "Invalid choice."        
+
+
+
+
